f74b4ceeecf08c9fdfecb1d4003e3f97549c0c24
Md Munna Mia <mdmunnamia987@gmail.com>
2025-04-26 20:43:50 +0600

    update: basic update on fetch method

--- internal/Services/services.go
+++ internal/Services/services.go
@@ -1,8 +1,12 @@
 package services
 
+import "fmt"
+
 type GhService struct{}
 
 func (g GhService) Fetch(userName string) (string, error) {
+	url := fmt.Sprint("https://api.github.com/users/%s/events", userName)
+	
 	return "", nil
 }
 
