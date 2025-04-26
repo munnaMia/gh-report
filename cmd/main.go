cd98ff9a0a0bffb3f82657399fd7f32240e25c8d
Md Munna Mia <mdmunnamia987@gmail.com>
2025-04-26 20:44:42 +0600

    basic work on main file

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main(){
+import (
+	"os"
+
+	services "github.com/munnaMia/gh-report/internal/Services"
+	"github.com/munnaMia/gh-report/internal/utils"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		utils.PrintFatal("Usage: gh-report <github-username>")
+	}
 	
+	username:= os.Args[2]
+
+	service := services.GhService{}
+
+	switch os.Args[1] {
+	case "gh-report":
+		utils.Must(service.Fetch(username))
+	}
 }
