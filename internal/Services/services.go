fed10b4c448ebe5f9505c00458165612d850f9db
Md Munna Mia <mdmunnamia987@gmail.com>
2025-04-28 20:17:04 +0600

    adding fetch method

--- internal/Services/services.go
+++ internal/Services/services.go
@@ -1,13 +1,45 @@
 package services
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/munnaMia/gh-report/internal/entity"
+	"github.com/munnaMia/gh-report/internal/utils"
+)
 
 type GhService struct{}
 
-func (g GhService) Fetch(userName string) (string, error) {
-	url := fmt.Sprint("https://api.github.com/users/%s/events", userName)
-	
-	return "", nil
+func (g GhService) Fetch(userName string) ([]entity.Activity, error) {
+	url := fmt.Sprintf("https://api.github.com/users/%s/events", userName)
+
+	response := utils.Must(http.Get(url))
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		fmt.Println("Error: HTTP status code", response.StatusCode)
+		os.Exit(1)
+	}
+
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		os.Exit(1)
+	}
+
+	var activity []entity.Activity
+
+	err = json.Unmarshal(body, &activity)
+	if err != nil {
+		fmt.Println("Error unmarshaling JSON:", err)
+		os.Exit(1)
+	}
+
+	return activity, nil
 }
 
-func (g GhService) Show() {}
+
